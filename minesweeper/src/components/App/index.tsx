f080553fd56c194221a501b3a7d2a995d6a7c36d
hamate <matehajdu2@gmail.com>
2021-01-14 12:58:46 +0100

    cells for fields created

--- minesweeper/src/components/App/index.tsx
+++ minesweeper/src/components/App/index.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NumberDisplay from '../NumberDisplay';
+import { generateCells } from '../../utils';
 import './App.scss';
+import Button from '../Button';
 
 const App: React.FC = () => {
+  const [cells, setCells] = useState(generateCells());
+
+  const renderCells = (): React.ReactNode => {
+    return cells.map((row, rowIndex) =>
+      row.map((cell, colIndex) => <Button key={`${rowIndex}`}/>)
+    );
+  };
+
   return (
     <div className="App">
       <div className="Header">
         <NumberDisplay value={0} />
-        <div className="Face"><span role='img' aria-label='face'>😁</span></div>
+        <div className="Face">
+          <span role="img" aria-label="face">
+            😁
+          </span>
+        </div>
         <NumberDisplay value={23} />
       </div>
-      <div className="Body">Body</div>
+      <div className="Body">{renderCells()}</div>
     </div>
   );
 };
